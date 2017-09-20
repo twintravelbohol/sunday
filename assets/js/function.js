b69d15fbaa386af3b19f7d5764dfebb38a7d9367
AE-Design <aedesignbohol@gmail.com>
2017-09-20 17:34:16 +0800

    Add files via upload

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,2 +1,12 @@
-$(document).ready( function() {
+$(document).ready( function () {
+   faqActive();
 })
+
+function faqActive() {
+   $('.faq-articles article').click( function() {
+      var $this = $(this);
+   
+      $this.addClass('faq-active');
+      $this.siblings().removeClass('faq-active');  
+   })
+}
