7d422cf6cf2f5c7e0fa5387266d61da5952d9643
AE-Design <aedesignbohol@gmail.com>
2017-10-05 08:51:52 +0800

    Add files via upload

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,1 +1,35 @@
 
+$(document).ready(function() {
+   sendMsg();
+   faqActive();
+});
+
+   function faqActive() {
+      $('.faq-articles article').click( function() {
+         var $this = $(this);
+
+         $this.addClass('faq-active');
+         $this.siblings().removeClass('faq-active');  
+      })
+   }
+
+   function sendMsg() {
+      $('.form').submit( function() {
+         event.preventDefault();
+
+         $.ajax({
+            url: 'https://formspree.io/'+siteEmail,
+            method: 'POST',
+            data: {
+               name: $("[name='name']").val(),
+               email: $("[name='email']").val(),
+               message: $("[name='message']").val(),
+            },
+            dataType: 'json'
+         }).done( function() {
+            alert('Message Submitted, Thank you and God Bless');
+         }).fail( function() {
+            alert('Message not Submitted, try use our contact information in the left, thank you');
+         })
+      })
+   }
